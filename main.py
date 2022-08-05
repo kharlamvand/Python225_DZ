18875dc32b4ff2209875542c67865cc3dd8efcb4
Andrey <nike.smith2@gmail.com>
2022-08-05 16:25:31 +0300

    Домашнее задание №10

--- main.py
+++ main.py
@@ -233,14 +233,16 @@
 #     if i in inp:
 #         print("YES")
 #
-s = ('2', '5', '3', '5', '2', '3', '6', '5', '1')
-for i in s:
-    print(i, end="")
-print()
-print("Количество 2 =", s.count('2'))
-print("Количество 5 =", s.count('5'))
-print("Количество 3 =", s.count('3'))
-print("Количество 6 =", s.count('6'))
-print("Количество 1 =", s.count('1'))
+# s = ('2', '5', '3', '5', '2', '3', '6', '5', '1')
+# for i in s:
+#     print(i, end="")
+# print()
+# print("Количество 2 =", s.count('2'))
+# print("Количество 5 =", s.count('5'))
+# print("Количество 3 =", s.count('3'))
+# print("Количество 6 =", s.count('6'))
+# print("Количество 1 =", s.count('1'))
 
-tes 
+# Домашнее задание №10
+
+print(" С github все ок")
