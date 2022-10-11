26cbcd84283b4a20c7da1f2591c259088a54b314
Andrey <nike.smith2@gmail.com>
2022-10-11 17:17:11 +0300

    Домашнее задание №25

--- main.py
+++ main.py
@@ -635,25 +635,301 @@
 
 # Домашнее задание №24
 
-class Conveyor:
+# class Conveyor:
+#
+#     def __init__(self, wt):
+#         self.__wt = wt
+#
+#     @property
+#     def wt(self):
+#         return self.__wt
+#
+#     @wt.setter
+#     def wt(self, wt):
+#         if isinstance(wt, int) or isinstance(wt, float):
+#             self.__wt = wt
+#         else:
+#             print("Вес должен быть целочисленным или вещественным числом")
+#
+#
+# c1 = Conveyor(12)
+# print(f"12 кг => {c1.wt * 2.20462262185:.2f} фунтов")
+#
+# c2 = Conveyor(41)
+# print(f"41 кг => {c2.wt * 2.20462262185:.3f} фунтов")
 
-    def __init__(self, wt):
-        self.__wt = wt
+
+# Домашнее задание №25
+
+# Вариант №1
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = 'RUB'
+#     suffix_usd = 'USD'
+#     suffix_eur = 'EUR'
+#
+#     def __init__(self, num, surname, percent, value=0):
+#         self.__num = num
+#         self.__surname = surname
+#         self.__percent = percent
+#         self.__value = value
+#         print(f'Счет #{self.__num} принадлежит {self.__surname} был открыт.')
+#         print('*' * 50)
+#
+#     def __del__(self):
+#         print('*' * 50)
+#         print(f'Счет #{self.__num} принадлежащий {self.__surname} был закрыт.')
+#         print()
+#
+#     def get_num(self):
+#         return self.__num
+#
+#     def set_num(self, num):
+#         self.__num = num
+#
+#     def get_surname(self):
+#         return self.__surname
+#
+#     def set_surname(self, surname):
+#         self.__surname = surname
+#
+#     def get_percent(self):
+#         return self.__percent
+#
+#     def set_percent(self, percent):
+#         self.__percent = percent
+#
+#     def get_value(self):
+#         return self.__value
+#
+#     def set_value(self, value):
+#         self.__value = value
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def edit_owner(self, surname):
+#         self.__surname = surname
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.__value, Account.rate_usd)
+#         print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.__value, Account.rate_eur)
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
+#
+#     def print_balance(self):
+#         print(f'Текущий баланс {self.__value} {Account.suffix}')
+#
+#     def print_info(self):
+#         print(f'Информация о счете:')
+#         print('-' * 20)
+#         print(f'#{self.__num}')
+#         print(f'Владелец: {self.__surname}')
+#         self.print_balance()
+#         print(f'Проценты: {self.__percent:.0%}')
+#         print('-' * 20)
+#
+#     def add_percents(self):
+#         self.__value += self.__value * self.__percent
+#         print('Проценты были успешно начислены')
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.__value:
+#             print(f'К сожалению у вас нет {val} {Account.suffix}')
+#         else:
+#             self.__value -= val
+#             print(f'{val} {Account.suffix} было успешно снято!')
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.__value += val
+#         print(f'{val} {Account.suffix} было успешно добавлено!')
+#         self.print_balance()
+#
+#
+# acc = Account('12345', 'Долгих', 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+#
+# Account.set_usd_rate(2)
+# acc.convert_to_usd()
+#
+# Account.set_eur_rate(3)
+# acc.convert_to_eur()
+# print()
+#
+# acc.edit_owner('Дюма')
+# acc.print_info()
+# print()
+#
+# acc.add_percents()
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
+#
+# acc.withdraw_money(100)
+# print()
+#
+# acc.add_money(5000)
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
+
+
+# Вариант №2
+
+class Account:
+    rate_usd = 0.013
+    rate_eur = 0.011
+    suffix = 'RUB'
+    suffix_usd = 'USD'
+    suffix_eur = 'EUR'
+
+    def __init__(self, num, surname, percent, value=0):
+        self.__num = num
+        self.__surname = surname
+        self.__percent = percent
+        self.__value = value
+        print(f'Счет #{self.__num} принадлежит {self.__surname} был открыт.')
+        print('*' * 50)
+
+    def __del__(self):
+        print('*' * 50)
+        print(f'Счет #{self.__num} принадлежащий {self.__surname} был закрыт.')
+        print()
 
     @property
-    def wt(self):
-        return self.__wt
+    def num(self):
+        return self.__num
 
-    @wt.setter
-    def wt(self, wt):
-        if isinstance(wt, int) or isinstance(wt, float):
-            self.__wt = wt
+    @num.setter
+    def num(self, num):
+        self.__num = num
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, surname):
+        self.__surname = surname
+
+    @property
+    def percent(self):
+        return self.__percent
+
+    @percent.setter
+    def percent(self, percent):
+        self.__percent = percent
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    @classmethod
+    def set_usd_rate(cls, rate):
+        cls.rate_usd = rate
+
+    @classmethod
+    def set_eur_rate(cls, rate):
+        cls.rate_eur = rate
+
+    @staticmethod
+    def convert(value, rate):
+        return value * rate
+
+    def edit_owner(self, surname):
+        self.__surname = surname
+
+    def convert_to_usd(self):
+        usd_val = Account.convert(self.__value, Account.rate_usd)
+        print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+
+    def convert_to_eur(self):
+        eur_val = Account.convert(self.__value, Account.rate_eur)
+        print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
+
+    def print_balance(self):
+        print(f'Текущий баланс {self.__value} {Account.suffix}')
+
+    def print_info(self):
+        print(f'Информация о счете:')
+        print('-' * 20)
+        print(f'#{self.__num}')
+        print(f'Владелец: {self.__surname}')
+        self.print_balance()
+        print(f'Проценты: {self.__percent:.0%}')
+        print('-' * 20)
+
+    def add_percents(self):
+        self.__value += self.__value * self.__percent
+        print('Проценты были успешно начислены')
+        self.print_balance()
+
+    def withdraw_money(self, val):
+        if val > self.__value:
+            print(f'К сожалению у вас нет {val} {Account.suffix}')
         else:
-            print("Вес должен быть целочисленным или вещественным числом")
+            self.__value -= val
+            print(f'{val} {Account.suffix} было успешно снято!')
+        self.print_balance()
+
+    def add_money(self, val):
+        self.__value += val
+        print(f'{val} {Account.suffix} было успешно добавлено!')
+        self.print_balance()
 
 
-c1 = Conveyor(12)
-print(f"12 кг => {c1.wt * 2.20462262185:.2f} фунтов")
+acc = Account('12345', 'Долгих', 0.03, 1000)
+acc.print_info()
+acc.convert_to_usd()
+acc.convert_to_eur()
+print()
 
-c2 = Conveyor(41)
-print(f"41 кг => {c2.wt * 2.20462262185:.3f} фунтов")
+
+Account.set_usd_rate(2)
+acc.convert_to_usd()
+
+Account.set_eur_rate(3)
+acc.convert_to_eur()
+print()
+
+acc.edit_owner('Дюма')
+acc.print_info()
+print()
+
+acc.add_percents()
+print()
+
+acc.withdraw_money(3000)
+print()
+
+acc.withdraw_money(100)
+print()
+
+acc.add_money(5000)
+print()
+
+acc.withdraw_money(3000)
+
