f212c5d2b875b6f642dce9e6a49c074a3c3c9aec
Andrey <nike.smith2@gmail.com>
2022-10-12 13:13:43 +0300

    Домашнее задание №26

--- main.py
+++ main.py
@@ -794,142 +794,204 @@
 
 
 # Вариант №2
+#
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = 'RUB'
+#     suffix_usd = 'USD'
+#     suffix_eur = 'EUR'
+#
+#     def __init__(self, num, surname, percent, value=0):
+#         self.__num = num
+#         self.__surname = surname
+#         self.__percent = percent
+#         self.__value = value
+#         print(f'Счет #{self.__num} принадлежит {self.__surname} был открыт.')
+#         print('*' * 50)
+#
+#     def __del__(self):
+#         print('*' * 50)
+#         print(f'Счет #{self.__num} принадлежащий {self.__surname} был закрыт.')
+#         print()
+#
+#     @property
+#     def num(self):
+#         return self.__num
+#
+#     @num.setter
+#     def num(self, num):
+#         self.__num = num
+#
+#     @property
+#     def surname(self):
+#         return self.__surname
+#
+#     @surname.setter
+#     def surname(self, surname):
+#         self.__surname = surname
+#
+#     @property
+#     def percent(self):
+#         return self.__percent
+#
+#     @percent.setter
+#     def percent(self, percent):
+#         self.__percent = percent
+#
+#     @property
+#     def value(self):
+#         return self.__value
+#
+#     @value.setter
+#     def value(self, value):
+#         self.__value = value
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def edit_owner(self, surname):
+#         self.__surname = surname
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.__value, Account.rate_usd)
+#         print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.__value, Account.rate_eur)
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
+#
+#     def print_balance(self):
+#         print(f'Текущий баланс {self.__value} {Account.suffix}')
+#
+#     def print_info(self):
+#         print(f'Информация о счете:')
+#         print('-' * 20)
+#         print(f'#{self.__num}')
+#         print(f'Владелец: {self.__surname}')
+#         self.print_balance()
+#         print(f'Проценты: {self.__percent:.0%}')
+#         print('-' * 20)
+#
+#     def add_percents(self):
+#         self.__value += self.__value * self.__percent
+#         print('Проценты были успешно начислены')
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.__value:
+#             print(f'К сожалению у вас нет {val} {Account.suffix}')
+#         else:
+#             self.__value -= val
+#             print(f'{val} {Account.suffix} было успешно снято!')
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.__value += val
+#         print(f'{val} {Account.suffix} было успешно добавлено!')
+#         self.print_balance()
+#
+#
+# acc = Account('12345', 'Долгих', 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+#
+#
+# Account.set_usd_rate(2)
+# acc.convert_to_usd()
+#
+# Account.set_eur_rate(3)
+# acc.convert_to_eur()
+# print()
+#
+# acc.edit_owner('Дюма')
+# acc.print_info()
+# print()
+#
+# acc.add_percents()
+# print()
+#
+# acc.withdraw_money(3000)
+# print()
+#
+# acc.withdraw_money(100)
+# print()
+#
+# acc.add_money(5000)
+# print()
+#
+# acc.withdraw_money(3000)
 
-class Account:
-    rate_usd = 0.013
-    rate_eur = 0.011
-    suffix = 'RUB'
-    suffix_usd = 'USD'
-    suffix_eur = 'EUR'
 
-    def __init__(self, num, surname, percent, value=0):
-        self.__num = num
-        self.__surname = surname
-        self.__percent = percent
-        self.__value = value
-        print(f'Счет #{self.__num} принадлежит {self.__surname} был открыт.')
-        print('*' * 50)
+# Домашнее задание №26
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
 
-    def __del__(self):
-        print('*' * 50)
-        print(f'Счет #{self.__num} принадлежащий {self.__surname} был закрыт.')
-        print()
+    def __str__(self):
+        return f'({self.__x}, {self.__y})'
 
-    @property
-    def num(self):
-        return self.__num
+    def is_digit(self):
+        if not isinstance(self.__x, (int, float)) or not isinstance(self.__y, (int, float)):
+            print("Координаты должны быть числами")
+            return False
+        return True
 
-    @num.setter
-    def num(self, num):
-        self.__num = num
-
-    @property
-    def surname(self):
-        return self.__surname
-
-    @surname.setter
-    def surname(self, surname):
-        self.__surname = surname
-
-    @property
-    def percent(self):
-        return self.__percent
-
-    @percent.setter
-    def percent(self, percent):
-        self.__percent = percent
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
-        self.__value = value
-
-    @classmethod
-    def set_usd_rate(cls, rate):
-        cls.rate_usd = rate
-
-    @classmethod
-    def set_eur_rate(cls, rate):
-        cls.rate_eur = rate
-
-    @staticmethod
-    def convert(value, rate):
-        return value * rate
-
-    def edit_owner(self, surname):
-        self.__surname = surname
-
-    def convert_to_usd(self):
-        usd_val = Account.convert(self.__value, Account.rate_usd)
-        print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
-
-    def convert_to_eur(self):
-        eur_val = Account.convert(self.__value, Account.rate_eur)
-        print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
-
-    def print_balance(self):
-        print(f'Текущий баланс {self.__value} {Account.suffix}')
-
-    def print_info(self):
-        print(f'Информация о счете:')
-        print('-' * 20)
-        print(f'#{self.__num}')
-        print(f'Владелец: {self.__surname}')
-        self.print_balance()
-        print(f'Проценты: {self.__percent:.0%}')
-        print('-' * 20)
-
-    def add_percents(self):
-        self.__value += self.__value * self.__percent
-        print('Проценты были успешно начислены')
-        self.print_balance()
-
-    def withdraw_money(self, val):
-        if val > self.__value:
-            print(f'К сожалению у вас нет {val} {Account.suffix}')
-        else:
-            self.__value -= val
-            print(f'{val} {Account.suffix} было успешно снято!')
-        self.print_balance()
-
-    def add_money(self, val):
-        self.__value += val
-        print(f'{val} {Account.suffix} было успешно добавлено!')
-        self.print_balance()
+    def is_int(self):
+        if not isinstance(self.__x, int) or not isinstance(self.__y, int):
+            print("Координаты должны быть целочисленными")
+            return False
+        return True
 
 
-acc = Account('12345', 'Долгих', 0.03, 1000)
-acc.print_info()
-acc.convert_to_usd()
-acc.convert_to_eur()
-print()
+class Prop:
+    def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
+        self._sp = sp
+        self._ep = ep
+        self._color = color
+        self._width = width
+
+    def set_coords(self, sp, ep):
+        if sp.is_digit() and ep.is_digit():
+            self._sp = sp
+            self._ep = ep
 
 
-Account.set_usd_rate(2)
-acc.convert_to_usd()
+class Line(Prop):
 
-Account.set_eur_rate(3)
-acc.convert_to_eur()
-print()
+    def draw_line(self) -> None:
+        print(f'Рисование линии: {self._sp}, {self._ep}, {self._color}, {self._width}')
 
-acc.edit_owner('Дюма')
-acc.print_info()
-print()
+    def set_coords(self, sp, ep):
+        if sp.is_int() and ep.is_int():
+            self._sp = sp
+            self._ep = ep
 
-acc.add_percents()
-print()
 
-acc.withdraw_money(3000)
-print()
+class Rect(Prop):
 
-acc.withdraw_money(100)
-print()
+    def draw_rect(self) -> None:
+        print(f'Рисование прямоугольника: {self._sp}, {self._ep}, {self._color}, {self._width}')
 
-acc.add_money(5000)
-print()
 
-acc.withdraw_money(3000)
+line = Line(Point(1, 2), Point(10, 20))
+line.draw_line()
+line.set_coords(Point(10.2, 20), Point(100, 200))
+line.draw_line()
 
+rect = Rect(Point(7, 9), Point(12, 15))
+rect.draw_rect()
+rect.set_coords(Point(30.5, 40.2), Point(50, 60))
+rect.draw_rect()
