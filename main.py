f83795d43b396dd9430b8c58ccde9287a758b75a
Andrey <nike.smith2@gmail.com>
2022-10-15 18:27:10 +0300

    Домашнее задание №28

--- main.py
+++ main.py
@@ -997,44 +997,70 @@
 # rect.draw_rect()
 
 # Домашнее задание №27
-class Liquid:
-    def __init__(self, name, density):
+# class Liquid:
+#     def __init__(self, name, density):
+#         self.name = name
+#         self.density = density
+#
+#     def edit_density(self, val):
+#         self.density = val
+#
+#     def calc_v(self, m):
+#         v = m / self.density
+#         print(f'Объем {m} кг {self.name} равен {v} m^3')
+#
+#     def calc_m(self, v):
+#         m = v * self.density
+#         print(f'Вес {v} m^3 of {self.name} составляет {m} кг.')
+#
+#     def print_info(self):
+#         print(f"Жидкость '{self.name}' (плотность = {self.density} kg/m^3).")
+#
+#
+# class Alcohol(Liquid):
+#     def __init__(self, name, density, strength):
+#         super().__init__(name, density)
+#         self.strength = strength
+#
+#     def edit_strength(self, val):
+#         self.strength = val
+#
+#
+# a = Alcohol('Wine', 1064.2, 14)
+# a.print_info()
+#
+# a.edit_density(1000)
+# a.print_info()
+#
+# a.calc_v(300)
+# a.calc_m(0.5)
+#
+# print(a.strength)
+# a.edit_strength(20)
+# print(a.strength)
+
+
+# Домашнее задание №28
+
+class Student:
+    def __init__(self, name):
         self.name = name
-        self.density = density
+        self.notebook = self.Notebook()
 
-    def edit_density(self, val):
-        self.density = val
+    class Notebook:
+        def make(self):
+            return "HP"
 
-    def calc_v(self, m):
-        v = m / self.density
-        print(f'Объем {m} кг {self.name} равен {v} m^3')
+        def model(self):
+            return "i7"
 
-    def calc_m(self, v):
-        m = v * self.density
-        print(f'Вес {v} m^3 of {self.name} составляет {m} кг.')
-
-    def print_info(self):
-        print(f"Жидкость '{self.name}' (плотность = {self.density} kg/m^3).")
+        def ram(self):
+            return "16"
 
 
-class Alcohol(Liquid):
-    def __init__(self, name, density, strength):
-        super().__init__(name, density)
-        self.strength = strength
-
-    def edit_strength(self, val):
-        self.strength = val
-
-
-a = Alcohol('Wine', 1064.2, 14)
-a.print_info()
-
-a.edit_density(1000)
-a.print_info()
-
-a.calc_v(300)
-a.calc_m(0.5)
-
-print(a.strength)
-a.edit_strength(20)
-print(a.strength)
+st1 = Student('Roman')
+my_notebook = st1.notebook
+print(f'{st1.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
+st2 = Student('Vladimir')
+my_notebook = st2.notebook
+print(f'{st2.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
