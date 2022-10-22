c5156bf46d05b23f787b73f3e95280ba8279d518
Andrey <nike.smith2@gmail.com>
2022-10-22 13:05:28 +0300

    Домашнее задание №29

--- main.py
+++ main.py
@@ -1042,25 +1042,114 @@
 
 # Домашнее задание №28
 
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.notebook = self.Notebook()
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+#         self.notebook = self.Notebook()
+#
+#     class Notebook:
+#         def make(self):
+#             return "HP"
+#
+#         def model(self):
+#             return "i7"
+#
+#         def ram(self):
+#             return "16"
+#
+#
+# st1 = Student('Roman')
+# my_notebook = st1.notebook
+# print(f'{st1.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
+# st2 = Student('Vladimir')
+# my_notebook = st2.notebook
+# print(f'{st2.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
 
-    class Notebook:
-        def make(self):
-            return "HP"
+# Домашнее задание №29
+class Clock:
+    __DAY = 86400
 
-        def model(self):
-            return "i7"
+    def __init__(self, sec: int):
+        if not isinstance(sec, int):
+            raise ValueError("Секунды должны быть целым числом")
 
-        def ram(self):
-            return "16"
+        self.sec = sec % self.__DAY
+
+    def __add__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return Clock(self.sec + other.sec)
+
+    def __sub__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError("Правый операнд должен быть типом данных Clock")
+        return Clock(self.sec - other.sec)
+
+    def __mul__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError()
+        return Clock(self.sec * other.sec)
+
+    def __floordiv__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError()
+        return Clock(self.sec // other.sec)
+
+    def __mod__(self, other):
+        if not isinstance(other, Clock):
+            raise ArithmeticError()
+        return Clock(self.sec % other.sec)
+
+    def __eq__(self, other):
+        return self.sec == other.sec
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return self.sec > other.sec
+
+    def get_format_time(self):
+        s = self.sec % 60  # секунды
+        m = (self.sec // 60) % 60  # минуты
+        h = (self.sec // 3600) % 24  # часы
+        return f'{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}'
+
+    @staticmethod
+    def __get_form(x):
+        return str(x) if x > 9 else "0" + str(x)
+
+    def __getitem__(self, item):
+        if not isinstance(item, str):
+            raise ValueError("Ключ должен быть строкой")
+
+        if item == "hour":
+            return (self.sec // 3600) % 24
+        elif item == "min":
+            return (self.sec // 60) % 60
+        elif item == "sec":
+            return self.sec % 60
+
+        return "Неверный ключ"
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, str):
+            raise ValueError("Ключ должен быть строкой")
+        if not isinstance(value, int):
+            raise ValueError("Значение должно быть целым числом ")
+        s = self.sec % 60  # секунды
+        m = (self.sec // 60) % 60  # минуты
+        h = (self.sec // 3600) % 24  # часы
+        if key == "hour":
+            self.sec = s + 60 * m + value * 3600
+        elif key == "min":
+            self.sec = s + 60 * value + h * 3600
+        elif key == "sec":
+            self.sec = value + 60 * m + h * 3600
 
 
-st1 = Student('Roman')
-my_notebook = st1.notebook
-print(f'{st1.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
-st2 = Student('Vladimir')
-my_notebook = st2.notebook
-print(f'{st2.name} => {my_notebook.make()}, {my_notebook.model()}, {my_notebook.ram()}')
+c1 = Clock(80000)
+print(c1.get_format_time())
+print(c1["hour"], c1["min"], c1["sec"])
+c1["hour"] = 10
+print(c1["hour"], c1["min"], c1["sec"])
